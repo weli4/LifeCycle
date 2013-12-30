7c4851030fcc2f19698667ba5705a77049aec2a3
weli4 <257877@gmail.com>
2013-12-30 17:35:21 +0300

    Initial commit

---
+++ webapp/js/right-bar.js
@@ -0,0 +1,48 @@
+var $workspace=$(".center-column"),
+    $processes=$(".processes"),
+    $process=$(".process");
+
+$(".glyphicon-folder-close").click(function(){
+    var div= $(this).parent().find("div")
+    if(div.css("display")!="none")
+    {
+        div.slideUp("fast");
+    }
+    else
+    {
+        div.slideDown("fast");
+    }
+})
+var trash_icon = "<span title='Delete this image' class='glyphicon glyphicon-remove'></span>";
+$(function() {
+    $process.draggable({ revert: "invalid" });
+
+    $workspace.droppable({
+        accept: ".processes div",
+        cancel: "span.glyphicon-remove",
+        drop: function( event, ui ) {
+            ui.draggable.append(trash_icon).appendTo($workspace).fadeIn()
+            var $newPosX = ui.offset.left - $(this).offset().left;
+            var $newPosY = ui.offset.top - $(this).offset().top;
+            ui.draggable.css({left: $newPosX})
+        }
+    });
+    $processes.droppable({
+        cancel: "span.glyphicon-remove",
+        accept: ".center-column div",
+        drop: function( event, ui ) {
+            ui.draggable.find("span.glyphicon-remove").remove().end().detach().css({top: 0,left: 0}).appendTo($processes);
+
+
+        }
+    });
+
+    $process.find(".glyphicon-remove").click(function(){
+        alert("ok");
+        $(this).parent().find("span.glyphicon-remove").remove();
+        $(this).parent().appendTo($processes);
+
+    })
+
+});
+
