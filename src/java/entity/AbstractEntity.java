98dcb45bc58884fec77546220ed15009164bc390
weli4 <257877@gmail.com>
2014-03-17 19:54:25 +0300

    .idea commit

--- src/java/entity/AbstractEntity.java
+++ src/java/entity/AbstractEntity.java
@@ -5,7 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-
 @MappedSuperclass
 public abstract class AbstractEntity {
     private Long id;
